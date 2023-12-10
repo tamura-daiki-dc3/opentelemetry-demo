a74363d777abb6d924b8cc0b9adc3b307994cff4
Daiki Tamura <tamura.daiki.dc3@jp.nssol.nipponsteel.com>
2023-12-10 07:58:46 +0000

    トレースの終了時点のコード

--- java-demo-app/src/main/java/com/dtamura/demo/DemoApplication.java
+++ java-demo-app/src/main/java/com/dtamura/demo/DemoApplication.java
@@ -5,6 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO import文を追加
+import org.springframework.context.annotation.Bean;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.ResourceAttributes;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
 
 // ここまで
 
@@ -17,6 +29,33 @@ public class DemoApplication {
 	}
 
 	// TODO OpenTelemetry初期化コードの追加
+	@Bean
+	public OpenTelemetry openTelemetry() {
+
+		// アプリケーションに関する属性情報をタグ付けする
+		Resource resource = Resource.getDefault().toBuilder()
+				.put(ResourceAttributes.SERVICE_NAME, DemoApplication.class.getName())
+				.put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
+
+		// OpenTelemetry CollectorにgRPCで送信するためのエクスポーター
+		OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().setEndpoint("http://otel-collector:4317")
+				.build();
+
+		// トレーサーを生成するTracerProviderを設定する。上記のエクスポーターを登録
+		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+				.addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build()) // Collectorに送る
+				.setResource(resource)
+				.build();
+
+		// TracerProviderを登録する
+		OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+				.setTracerProvider(sdkTracerProvider)
+				.setPropagators(ContextPropagators.create(TextMapPropagator
+						.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
+				.build();
+
+		return openTelemetry;
+	}
 
 	// ここまで
 
--- java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
+++ java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO import文を追加ここから
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import java.util.ArrayList;
+import java.util.List;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 // ここまで
 
@@ -20,21 +30,55 @@ public class GreetingController {
    private Logger logger = LogManager.getLogger(GreetingController.class.getName());
 
    // TODO Tracerプライベート変数を追加
+   private Tracer tracer;
+   private OpenTelemetry openTelemetry;
+
+   // HTTPヘッダから、トレースID等の情報を抽出するために使われるgetter関数
+   private final TextMapGetter<HttpHeaders> getter = new TextMapGetter<HttpHeaders>() {
+      @Override
+      public String get(HttpHeaders headers, String s) {
+         assert headers != null;
+         List<String> list = headers.get(s);
+         if (list != null) {
+            return list.get(0);
+         }
+         return null;
+      }
+
+      @Override
+      public Iterable<String> keys(HttpHeaders headers) {
+         List<String> keys = new ArrayList<>();
+         headers.forEach((k, v) -> {
+            keys.add(k);
+         });
+         return keys;
+      }
+   };
 
    // TODO コンストラクタの引数にOpenTelemetryを追加
    // TODO コンストラクタ内でtracer変数を初期化
-   public GreetingController() {
+   public GreetingController(OpenTelemetry openTelemetry) {
+      this.openTelemetry = openTelemetry;
+      this.tracer = openTelemetry.getTracer(GreetingController.class.getName(), "0.1.0");
    }
 
    @GetMapping("/greeting")
-   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+         @RequestHeader HttpHeaders headers) {
 
       // TODO スパンの開始コードを追加
+      Context extractedContext = this.openTelemetry.getPropagators().getTextMapPropagator()
+            .extract(Context.current(), headers, this.getter);
+      Span span = tracer.spanBuilder("greeting").setParent(extractedContext).startSpan();
 
       logger.info("start greeting");
 
-      hoge();
-      // TODO スパンの終了コードを追加
+      try (Scope scope = span.makeCurrent()) {
+         hoge();
+      } finally {
+         // TODO スパンの終了コードを追加
+         span.end();
+      }
 
       logger.info("end greeting");
       return new Greeting(counter.incrementAndGet(), String.format(template, name));
@@ -42,7 +86,10 @@ public class GreetingController {
 
    public void hoge() {
       // TODO スパンの開始コードを追加
+      Span span = tracer.spanBuilder("hoge").startSpan();
+
       logger.info("hoge");
       // TODO スパンの終了コードを追加
+      span.end();
    }
 }
