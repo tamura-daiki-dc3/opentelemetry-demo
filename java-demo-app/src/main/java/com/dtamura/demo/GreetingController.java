a0be7d05a8a7f00389d9a0a5b42ddc4d8c1ca1d9
Daiki Tamura <tamura.daiki.dc3@jp.nssol.nipponsteel.com>
2023-12-10 05:58:41 +0000

    ヘッダからコンテキスト読み取り

--- java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
+++ java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
@@ -1,17 +1,24 @@
 package com.dtamura.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapGetter;
 
 @RestController
 public class GreetingController {
@@ -19,23 +26,51 @@ public class GreetingController {
    private static final String template = "Hello, %s!";
    private final AtomicLong counter = new AtomicLong();
 
-   private Tracer tracer;
+   private final Tracer tracer;
+   private OpenTelemetry openTelemetry;
 
    private Logger logger = LogManager.getLogger(GreetingController.class.getName());
+   private final TextMapGetter<HttpHeaders> getter = new TextMapGetter<HttpHeaders>() {
+      @Override
+      public String get(HttpHeaders headers, String s) {
+         assert headers != null;
+         List<String> list = headers.get(s);
+         if (list != null) {
+            return list.get(0);
+         }
+         return null;
+      }
+
+      @Override
+      public Iterable<String> keys(HttpHeaders headers) {
+         List<String> keys = new ArrayList<>();
+         headers.forEach((k, v) -> {
+            keys.add(k);
+         });
+         return keys;
+      }
+   };
 
    public GreetingController(OpenTelemetry openTelemetry) {
+      this.openTelemetry = openTelemetry;
       this.tracer = openTelemetry.getTracer(GreetingController.class.getName(), "0.1.0");
    }
 
    @GetMapping("/greeting")
-   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+         @RequestHeader HttpHeaders headers) {
 
       logger.info("start greeting");
 
-      Span span = tracer.spanBuilder("hello").startSpan();
+      Context extractedContext = this.openTelemetry.getPropagators().getTextMapPropagator()
+            .extract(Context.current(), headers, this.getter);
+      Span span = tracer.spanBuilder("hello").setSpanKind(SpanKind.SERVER)
+            .setParent(extractedContext).startSpan();
       try (Scope scope = span.makeCurrent()) {
          // span
          hoge();
+         span.setAttribute(io.opentelemetry.semconv.SemanticAttributes.HTTP_RESPONSE_STATUS_CODE, 200);
+         span.setAttribute(io.opentelemetry.semconv.SemanticAttributes.HTTP_REQUEST_METHOD, "GET");
       } finally {
          span.end();
       }
