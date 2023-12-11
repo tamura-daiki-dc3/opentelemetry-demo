6f32407d34acc8c3dffd656a4a655c6445a45000
Shu Adachi <72395715+LgmQue@users.noreply.github.com>
2023-12-11 10:31:15 +0900

    Update GreetingController.java
    Metric追加

--- java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
+++ java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
@@ -12,6 +12,8 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 
 @RestController
 public class GreetingController {
@@ -20,11 +22,19 @@ public class GreetingController {
    private final AtomicLong counter = new AtomicLong();
 
    private Tracer tracer;
+   private Meter meter;
+   private LongCounter longCounter;
 
    private Logger logger = LogManager.getLogger(GreetingController.class.getName());
 
    public GreetingController(OpenTelemetry openTelemetry) {
       this.tracer = openTelemetry.getTracer(GreetingController.class.getName(), "0.1.0");
+      // メトリクスの設定
+      this.meter = openTelemetry.getMeterProvider().get(GreetingController.class.getName());
+      this.longCounter = meter.counterBuilder("greeting_requests")
+                     .setDescription("Total number of greeting requests")
+                     .setUnit("1")
+                     .build();
    }
 
    @GetMapping("/greeting")
@@ -39,6 +49,9 @@ public class GreetingController {
       } finally {
          span.end();
       }
+
+      // カウンターを増やす
+      longCounter.add(1);
 
       logger.info("end greeting");
       return new Greeting(counter.incrementAndGet(), String.format(template, name));
